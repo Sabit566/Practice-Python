6e30b1fc66ce2c5f7923fda8e1d0f8da765a9534
Ahammad Ullah <sabit.islam566@gmail.com>
2026-07-15 15:55:09 +0600

    2nd day

---
+++ 3.modifyString.py
@@ -0,0 +1,25 @@
+print("\n")
+
+#Upper Case
+
+txt = "hello world"
+
+print(f"Upper case: {txt.upper()}")
+
+print("\n")
+
+#Lower Case
+
+txt = "hElLo WOrLd"
+
+print(f"Lower case: {txt.lower()}")
+
+print("\n")
+
+#Remove Whitespace from the beginning or the end
+
+txt = "  Harvard, Roosevelt  "
+
+print(f"Remove Whitespace: {txt.strip()}")
+
+print("\n")
