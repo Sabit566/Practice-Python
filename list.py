52396cf5349bc6b6d49a8a569ef372380a9a51fd
Ahammad Ullah <sabit.islam566@gmail.com>
2026-07-16 14:35:32 +0600

    3rd day

--- list.py
+++ list.py
@@ -40,3 +40,45 @@ if search_name in mylist:
     print(f"The name {search_name} is present in list")
 else:
     print(f"The name {search_name} is not present in list")
+
+
+#Change Item Value
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim']
+mylist[1] = "Jayanto"
+print(f"The list after change: {mylist}")
+
+#Change a Range of Item Values
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim']
+mylist[1:3] = ["Jayanto","Kabbo"]
+print(f"The list after change using range: {mylist}")
+
+#Append Items -To add an item to the end of the list
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim']
+thislist.append("Jayanto")
+print(f"The list after appending to list: {thislist}")
+
+#Insert Items -To insert a list item at a specified index
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim']
+mylist.insert(2, "Jayanto")
+print(f"The list after inserting to list: {mylist}")
+
+#Extend List -To append elements from another list to the current list
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim']
+yourlist = ['Alok','Fardin','Badhon','Kabbo']
+
+mylist.extend(yourlist)
+print(f"The list after extending to list: {mylist}")
+
+#Add Any Iterable -add any iterable object (tuples, sets, dictionaries)
+
+thislist = ['Sabit','Tanvir','Ovi','Wasim']
+thistuple = ("Kabbo","Fardin","Badhon")
+
+thislist.extend(thistuple)
+
+print(f"The list after extending to tuple: {thislist}")
