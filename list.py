67890e49ecda514cdc90caca0f6c33210a96e4df
Ahammad Ullah <sabit.islam566@gmail.com>
2026-07-16 11:22:24 +0600

    3rd day

---
+++ list.py
@@ -0,0 +1,17 @@
+#List - allow duplicates, contain different data types
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim','Sabit']
+print(f"List: {mylist}")
+
+#List Length
+
+mylist = ['Sabit','Tanvir','Ovi','Wasim','Sabit']
+print(f"Length of list: {len(mylist)}")
+
+#Check list class
+mylist = ['Sabit','Tanvir','Ovi','Wasim']
+print(f"Check type: {type(mylist)}")
+
+#The list() Constructor
+thislist = list(("apple","banana","Mango"))
+print(f"Using list constructor: {thislist}")
